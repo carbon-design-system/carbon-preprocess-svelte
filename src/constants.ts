660a4819b3d625f41f8f38face9089e2e5bad142
Eric Liu <ericyl.us@gmail.com>
2021-05-08 04:49:40 -0700

    refactor: omit path.join

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { join } from "path";
-
 export const CARBON_SVELTE = {
   components: "carbon-components-svelte",
   icons: "carbon-icons-svelte",
@@ -12,10 +10,10 @@ export const LATEST_MAJOR_VERSION = {
   [CARBON_SVELTE.pictograms]: "11",
 };
 
-export const API_COMPONENTS = join(__dirname, `${CARBON_SVELTE.components}.js`);
-export const API_ELEMENTS = join(__dirname, "carbon-elements.js");
-export const API_ICONS = join(__dirname, "carbon-icons.js");
-export const API_PICTOGRAMS = join(__dirname, "carbon-pictograms.js");
+export const API_COMPONENTS = `src/${CARBON_SVELTE.components}.js`;
+export const API_ELEMENTS = "src/carbon-elements.js";
+export const API_ICONS = "src/carbon-icons.js";
+export const API_PICTOGRAMS = "src/carbon-pictograms.js";
 
 export const EXT_SVELTE = /\.(svelte)$/;
 export const EXT_CSS = /\.(css)$/;
