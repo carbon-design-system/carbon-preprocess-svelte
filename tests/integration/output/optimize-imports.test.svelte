c2171ecdbb8c868ad648468699b7c4f76ecdb251
Eric Liu <ericyl.us@gmail.com>
2021-07-05 17:08:58 -0700

    Upgrade carbon-components-svelte to v0.39 (#14)
    * chore(deps-dev): upgrade carbon-components-svelte to v0.39
    
    * test(optimize-imports): test new components

--- tests/integration/output/optimize-imports.test.svelte
+++ tests/integration/output/optimize-imports.test.svelte
@@ -2,6 +2,9 @@
   import Accordion2 from "carbon-components-svelte/src/Accordion/Accordion.svelte";
   import Button from "carbon-components-svelte/src/Button/Button.svelte";
 
+  import RecursiveList from "carbon-components-svelte/src/RecursiveList/RecursiveList.svelte";
+import TreeView from "carbon-components-svelte/src/TreeView/TreeView.svelte";
+import ProgressBar from "carbon-components-svelte/src/ProgressBar/ProgressBar.svelte";
   import Add16 from "carbon-icons-svelte/lib/Add16/Add16.svelte";
 import Add20 from "carbon-icons-svelte/lib/Add20/Add20.svelte";
   import Airplane from "carbon-pictograms-svelte/lib/Airplane.svelte";
