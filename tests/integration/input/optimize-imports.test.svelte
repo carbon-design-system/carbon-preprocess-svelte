88e0d432292a525df79abc6dc58b541e369c1b5d
Eric Y Liu <ericyl.us@gmail.com>
2021-07-11 17:23:56 -0700

    test(integration): update optimizeImports snapshot

--- tests/integration/input/optimize-imports.test.svelte
+++ tests/integration/input/optimize-imports.test.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Accordion as Accordion2 } from "carbon-components-svelte";
   import { Button, Button2 } from "carbon-components-svelte";
-  import { RecursiveList, TreeView, ProgressBar } from "carbon-components-svelte";
+  import { RecursiveList, TreeView, ProgressBar, Theme, Breakpoint } from "carbon-components-svelte";
   import { Add16, Add20 } from "carbon-icons-svelte";
   import { Airplane } from "carbon-pictograms-svelte";
   import { Bridge, Item } from "carbon-pictograms-svelte";
