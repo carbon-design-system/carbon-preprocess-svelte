f59f2c1edb02a1955f5d16ee5a4ee042569f94ed
Eric Liu <ericyl.us@gmail.com>
2024-07-28 13:08:50 -0700

    test: use assertions instead of snapshot

--- tests/create-optimized-css.test.ts
+++ tests/create-optimized-css.test.ts
@@ -3,68 +3,91 @@ import { createOptimizedCss } from "../src/plugins/create-optimized-css";
 describe("create-optimized-css", () => {
   test("removes unused selectors", () => {
     const result = createOptimizedCss({
-      source: `
-        * { box-sizing: border-box }
-        a { color: blue }
-        button.bx--btn { background-color: red }
-        .bx--btn, .bx--btn--primary { color: white }
-        .bx--accordion { background-color: yellow }
-        .bx--accordion--end, .bx--accordion__content {color: black }
-      `,
+      source: `* { box-sizing: border-box }
+a { color: blue }
+button.bx--btn { background-color: red }
+.bx--btn, .bx--btn--primary { color: white }
+.bx--accordion { background-color: yellow }
+.bx--accordion--end, .bx--accordion__content {color: black }`,
       ids: ["/Accordion.svelte"],
     });
-    expect(result).toMatchSnapshot();
+    expect(result).toEqual(`* { box-sizing: border-box }
+a { color: blue }
+.bx--accordion { background-color: yellow }
+.bx--accordion--end, .bx--accordion__content {color: black }`)
   });
 
-  const font_rules = `
-    @font-face {
-      font-family: 'CustomFont';
-      src: url('path/to/custom-font.ttf');
-    }
-
-    @font-face {
-      font-display: auto;
-      font-family: IBM Plex Mono;
-      font-style: normal;
-      font-weight: 400;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: normal;
-      font-weight: 600;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: normal;
-      font-weight: 400;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: normal;
-      font-weight: 300;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: normal;
-      font-weight: 700;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: italic;
-      font-weight: 400;
-    }
-    @font-face {
-      font-family: IBM Plex Sans;
-      font-style: normal;
-      font-weight: 100;
-    }`;
+  const font_rules = `@font-face {
+  font-family: 'CustomFont';
+  src: url('path/to/custom-font.ttf');
+}
+@font-face {
+  font-display: auto;
+  font-family: IBM Plex Mono;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 300;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 700;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: italic;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 100;
+}`;
 
   test("removes unused @font rules", () => {
     const result = createOptimizedCss({
       source: font_rules,
       ids: ["/Accordion.svelte"],
     });
-    expect(result).toMatchSnapshot();
+    expect(result).toEqual(`@font-face {
+  font-family: 'CustomFont';
+  src: url('path/to/custom-font.ttf');
+}
+@font-face {
+  font-display: auto;
+  font-family: IBM Plex Mono;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 300;
+}`)
   });
 
   test("preserves all IBM fonts", () => {
@@ -73,6 +96,45 @@ describe("create-optimized-css", () => {
       ids: ["/Accordion.svelte"],
       preserveAllIBMFonts: true,
     });
-    expect(result).toMatchSnapshot();
+    expect(result).toEqual(`@font-face {
+  font-family: 'CustomFont';
+  src: url('path/to/custom-font.ttf');
+}
+@font-face {
+  font-display: auto;
+  font-family: IBM Plex Mono;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 300;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 700;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: italic;
+  font-weight: 400;
+}
+@font-face {
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 100;
+}`)
   });
 });
