dce253298741c01c7da56b15420e73802b50aede
Eric Liu <ericyl.us@gmail.com>
2024-07-28 13:08:50 -0700

    test: use assertions instead of snapshot

--- tests/optimize-imports.test.ts
+++ tests/optimize-imports.test.ts
@@ -25,21 +25,30 @@ describe("optimizeImports", () => {
   test("barrel imports", () => {
     expect(
       preprocess({
-        content: `
-        import { Accordion, AccordionItem } from "carbon-components-svelte";
-        import { Accordion as Accordion2 } from "carbon-components-svelte";
-        import { breakpoints } from "carbon-components-svelte";
-        import { invalid } from "carbon-components-svelte";
+        content: `import { Accordion, AccordionItem } from "carbon-components-svelte";
+import { Accordion as Accordion2 } from "carbon-components-svelte";
+import { breakpoints } from "carbon-components-svelte";
+import { invalid } from "carbon-components-svelte";
 
-        import { Add } from "carbon-icons-svelte";
-        import { Add as Add2 } from "carbon-icons-svelte";
-        import Add3 from "carbon-icons-svelte/lib/Add.svelte";
+import { Add } from "carbon-icons-svelte";
+import { Add as Add2 } from "carbon-icons-svelte";
+import Add3 from "carbon-icons-svelte/lib/Add.svelte";
 
-        import { Airplane } from "carbon-pictograms-svelte";
-        import { Airplane as Airplane2 } from "carbon-pictograms-svelte";
-        import Airplane3 from "carbon-pictograms-svelte/lib/Airplane.svelte";
-    `,
+import { Airplane } from "carbon-pictograms-svelte";
+import { Airplane as Airplane2 } from "carbon-pictograms-svelte";
+import Airplane3 from "carbon-pictograms-svelte/lib/Airplane.svelte";`,
       }),
-    ).toMatchSnapshot();
+    ).toEqual(`import Accordion from "carbon-components-svelte/src/Accordion/Accordion.svelte";import AccordionItem from "carbon-components-svelte/src/Accordion/AccordionItem.svelte";
+import Accordion2 from "carbon-components-svelte/src/Accordion/Accordion.svelte";
+import breakpoints from "carbon-components-svelte/src/Breakpoint/breakpoints.js";
+import { invalid } from "carbon-components-svelte";
+
+import Add from "carbon-icons-svelte/lib/Add.svelte";
+import Add2 from "carbon-icons-svelte/lib/Add.svelte";
+import Add3 from "carbon-icons-svelte/lib/Add.svelte";
+
+import Airplane from "carbon-pictograms-svelte/lib/Airplane.svelte";
+import Airplane2 from "carbon-pictograms-svelte/lib/Airplane.svelte";
+import Airplane3 from "carbon-pictograms-svelte/lib/Airplane.svelte";`)
   });
 });
