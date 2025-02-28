5341e04e9c7f01256c72b738598514f10a3a5b32
Eric Liu <ericyl.us@gmail.com>
2025-02-28 11:33:04 -0800

    chore(examples): add `type` import test case

--- examples/webpack@svelte-5/src/App.svelte
+++ examples/webpack@svelte-5/src/App.svelte
@@ -1,59 +1,65 @@
 <script lang="ts">
 import "carbon-components-svelte/css/g100.css";
 import { DataTable } from "carbon-components-svelte";
-</script>
+import type { DataTableProps } from "carbon-components-svelte/src/DataTable/DataTable.svelte";
 
-<DataTable
-  sortable
-  selectable
-  headers={[
-    { key: "name", value: "Name" },
-    { key: "protocol", value: "Protocol" },
-    { key: "port", value: "Port" },
-    { key: "rule", value: "Rule" },
-  ]}
-  rows={[
-    {
-      id: "a",
-      name: "Load Balancer 3",
-      protocol: "HTTP",
-      port: 3000,
-      rule: "Round robin",
-    },
-    {
-      id: "b",
-      name: "Load Balancer 1",
-      protocol: "HTTP",
-      port: 443,
-      rule: "Round robin",
-    },
-    {
-      id: "c",
-      name: "Load Balancer 2",
-      protocol: "HTTP",
-      port: 80,
-      rule: "DNS delegation",
-    },
-    {
-      id: "d",
-      name: "Load Balancer 6",
-      protocol: "HTTP",
-      port: 3000,
-      rule: "Round robin",
-    },
-    {
-      id: "e",
-      name: "Load Balancer 4",
-      protocol: "HTTP",
-      port: 443,
-      rule: "Round robin",
-    },
-    {
-      id: "f",
-      name: "Load Balancer 5",
-      protocol: "HTTP",
-      port: 80,
-      rule: "DNS delegation",
-    },
-  ]}
-/>
+let tableProps: DataTableProps = {
+  sortable: true,
+  selectable: true,
+};
+</script>
+  
+  <DataTable
+    {...tableProps}
+    headers={[
+      { key: "name", value: "Name" },
+      { key: "protocol", value: "Protocol" },
+      { key: "port", value: "Port" },
+      { key: "rule", value: "Rule" },
+    ]}
+    rows={[
+      {
+        id: "a",
+        name: "Load Balancer 3",
+        protocol: "HTTP",
+        port: 3000,
+        rule: "Round robin",
+      },
+      {
+        id: "b",
+        name: "Load Balancer 1",
+        protocol: "HTTP",
+        port: 443,
+        rule: "Round robin",
+      },
+      {
+        id: "c",
+        name: "Load Balancer 2",
+        protocol: "HTTP",
+        port: 80,
+        rule: "DNS delegation",
+      },
+      {
+        id: "d",
+        name: "Load Balancer 6",
+        protocol: "HTTP",
+        port: 3000,
+        rule: "Round robin",
+      },
+      {
+        id: "e",
+        name: "Load Balancer 4",
+        protocol: "HTTP",
+        port: 443,
+        rule: "Round robin",
+      },
+      {
+        id: "f",
+        name: "Load Balancer 5",
+        protocol: "HTTP",
+        port: 80,
+        rule: "DNS delegation",
+      },
+    ]}
+  />
+  
